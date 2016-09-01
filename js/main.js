521844f445f066ad2ce4c2e5b10a5517267b4090
AdriaCompte <adria.ros.compte@hp.com>
2016-09-01 17:18:07 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@ if ('serviceWorker' in navigator) {
  console.log('Service Worker is supported');
  navigator.serviceWorker.register('/push-notification/js/sw.js').then(function(reg) {
    console.log(':^)', reg);
-   // TODO
+   reg.pushManager.subscribe({
+            userVisibleOnly: true
+        }).then(function(sub) {
+            console.log('endpoint:', sub.endpoint);
+        });
  }).catch(function(err) {
    console.log(':^(', err);
  });
